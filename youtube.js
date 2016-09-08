20c593586a3208f2e4f1b61633e9f0a0992d22a9
Nick Cox <nickcox1008@gmail.com>
2016-09-08 11:50:46 -0600

    Update video list selectors

--- youtube.js
+++ youtube.js
@@ -13,7 +13,9 @@ function get_a(id, text) {
 	var a = document.createElement('a');
 	a.href = 'https://youtube.com/embed/' + id + '?autoplay=1';
 	a.title = 'Full Window Video';
+	a.style.textDecoration = 'initial';
 	a.style.fontSize = '20px';
+	a.style.color = '#167ac6';
 	a.appendChild(document.createTextNode(text));
 	return a;
 }
@@ -28,7 +30,7 @@ function list(selector, num_parents) {
 		}
 		var a = get_a(id, '⊕');
 		a.style.position = 'absolute';
-		a.style.bottom = '5px';
+		a.style.bottom = '0';
 		a.style.right = '0';
 		
 		var par = links[i];
@@ -42,11 +44,18 @@ function list(selector, num_parents) {
 function run() {
 	// Subscriptions list
 	list('div.yt-lockup-video h3.yt-lockup-title a.yt-uix-sessionlink', 2);
+	// Subscriptions list - September 2016
+	list('ytd-grid-video-renderer div.ytd-grid-video-renderer a.ytd-video-meta-block', 2);
+	
 	// Suggestions sidebar list
 	list('ul.video-list li.video-list-item a.yt-uix-sessionlink', 2);
+	// Suggestions sidebar list - September 2016
+	list('div.ytd-watch-next-secondary-results-renderer a.ytd-compact-video-renderer', 1);
+	
 	// Watch later list
 	list('tr.pl-video td.pl-video-title a.yt-uix-sessionlink', 1);
-	// On a video page
+	
+	// Main video on a page
 	var video_id = get_id(window.location.search);
 	if (!video_id) {
 		return;
@@ -54,11 +63,18 @@ function run() {
 	var a = get_a(video_id, ' - - ⊕');
 	a.style.verticalAlign = 'middle';
 	var spot = document.getElementById('watch7-subscription-container');
-	spot.appendChild(a);
+	if (spot) {
+		spot.appendChild(a);
+	}
+	// New spot as of September 2016
+	spot = document.querySelectorAll('ytd-video-primary-info-renderer h1.title yt-formatted-string')[0];
+	if (spot) {
+		spot.appendChild(a);
+	}
 }
 
 jQuery(function($) {
-	run();
+	setTimeout(run, 1000);
 	
 	// YouTube asynchronous page navigation.
 	// http://stackoverflow.com/questions/18397962/chrome-extension-is-not-loading-on-browser-navigation-at-youtube/18398921#18398921
